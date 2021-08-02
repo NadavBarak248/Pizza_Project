2278c99b734aa602d23ee54129df71a42fb5ef4d
nadav <nadav@NADAVB-PC>
2021-08-02 17:15:28 +0300

    more validations

--- Store_Project/Models/User.cs
+++ Store_Project/Models/User.cs
@@ -16,6 +16,7 @@ namespace Store_Project.Models
         public int Id { get; set; }
 
         [Required(ErrorMessage = "you must enter the user name")]
+        [RegularExpression("[a-zA-Z0-9]*", ErrorMessage = "username can contain letters and digits only")]
         public String Username { get; set; }
 
         [RegularExpression("^[A-Z]+[a-zA-Z0-9]*$", ErrorMessage = "password must start with a capital letter ")] 
