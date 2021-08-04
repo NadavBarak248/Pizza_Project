6f1d1ea696b1c418c81833c5820c996fd041ff1d
nadav <nadav@NADAVB-PC>
2021-08-04 17:42:15 +0300

    User CRUD

---
+++ Store_Project/wwwroot/js/userSearch.js
@@ -0,0 +1,39 @@
+﻿function update_page(page_data) {
+    $('tbody').html('');
+
+    var template = $('#tableSearchTemplate').html();
+
+    $.each(page_data, function (i, val) {
+        
+        var temp = template;
+
+        $.each(val, function (key, value) {
+            temp = temp.replaceAll('{' + key + '}', value);
+        });
+
+        $('tbody').append(temp);
+    });
+}
+
+function handle_data(e) {
+    e.preventDefault();
+    var searchval = $('#searchQuery').val();
+
+    $.ajax({
+        type: 'post',
+        url: "/Users/Search",
+        dataType: 'JSON',
+        data: {
+            searchquery: searchval
+        }
+    }).done(function (ret_data) {
+        update_page(ret_data);
+    });
+}
+
+
+$(function () {
+    $('#searchQuery').keyup(function (e) {
+        handle_data(e);
+    });
+});
